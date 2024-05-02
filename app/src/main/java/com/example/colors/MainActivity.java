ec7746bb1f73c16a82980f00eafb65a5d97b6a8e
dz2244 <dz2244@bs.amalnet.k12.il>
2024-05-03 00:56:38 +0300

    a

--- app/src/main/java/com/example/colors/MainActivity.java
+++ app/src/main/java/com/example/colors/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.colors;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    Switch switch1;
+    RadioButton rbRed, rbGreen ,rbBlue,rbWhite;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
+
+    private void initViews() {
+        switch1 = findViewById(R.id.switch1);
+        rbRed = findViewById(R.id.rbRed);
+        rbGreen = findViewById(R.id.rbGreen);
+        rbBlue = findViewById(R.id.rbBlue);
+        rbWhite = findViewById(R.id.rbWhite);
+        btn = findViewById(R.id.btn);
+
+    }
+
 }
