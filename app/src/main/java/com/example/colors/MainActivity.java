afeeaeb3346dc8284d1b5761ea56b917d85bd31e
dz2244 <dz2244@bs.amalnet.k12.il>
2024-05-06 22:24:40 +0300

    done

--- app/src/main/java/com/example/colors/MainActivity.java
+++ app/src/main/java/com/example/colors/MainActivity.java
@@ -7,20 +7,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.Switch;
-
 public class MainActivity extends AppCompatActivity {
     Switch switch1;
     RadioButton rbRed, rbGreen ,rbBlue,rbWhite;
     Button btn;
     ConstraintLayout background;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
     }
-
     private void initViews() {
         switch1 = findViewById(R.id.switch1);
         rbRed = findViewById(R.id.rbRed);
@@ -29,41 +26,53 @@ public class MainActivity extends AppCompatActivity {
         rbWhite = findViewById(R.id.rbWhite);
         btn = findViewById(R.id.btn);
         background = findViewById(R.id.background);
-
     }
-
-    public void clickedSwitch(View view) {
-    }
-
-    public void clickedRbRed(View view) {
+    public void clickedSwitch(View view) {}
+    public void clickedRbRed(View view)
+    {
         if(switch1.isChecked())
         {
             background.setBackgroundColor(Color.RED);
         }
     }
-
-    public void clickedRbGreen(View view) {
+    public void clickedRbGreen(View view)
+    {
         if(switch1.isChecked())
         {
             background.setBackgroundColor(Color.GREEN);
         }
     }
-
-    public void clickedRbBlue(View view) {
+    public void clickedRbBlue(View view)
+    {
         if(switch1.isChecked())
         {
             background.setBackgroundColor(Color.BLUE);
         }
     }
-
-    public void clickedRbWhite(View view) {
+    public void clickedRbWhite(View view)
+    {
         if(switch1.isChecked())
         {
             background.setBackgroundColor(Color.WHITE);
         }
     }
-
-    public void clickedbtn(View view) {
-
+    public void clickedbtn(View view)
+    {
+        if(rbRed.isChecked())
+        {
+            background.setBackgroundColor(Color.RED);
+        }
+        else if(rbBlue.isChecked())
+        {
+            background.setBackgroundColor(Color.BLUE);
+        }
+        else if(rbGreen.isChecked())
+        {
+            background.setBackgroundColor(Color.GREEN);
+        }
+        else
+        {
+            background.setBackgroundColor(Color.WHITE);
+        }
     }
 }
